50755616f834f102b8857c746fcf927791fb6d91
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-04-17 14:21:36 +0200

    Code update

--- bc.py
+++ bc.py
@@ -16,20 +16,24 @@ if __name__ == "__main__":
 
   items = cdisc_bc_library.catalogue()
   bc_list = []
-  for item in items:
+  for index, item in enumerate(items):
     name = item.title()
+    filename = name.lower().replace(' ', '_')
+    filename = filename.replace('/', '-')
+
     syn = cdisc_bc_library.synonyms(item)
     bc_list.append({ 'name': name, 'synonyms': syn})
     print("BC:", name)
-  with open(f"bc_data/catalogue.yaml", 'w', encoding='utf-8') as f:
-    yaml.dump(bc_list, f, default_flow_style=False)
-  for item in items:
-    name = item.title()
-    filename = name.lower().replace(' ', '_')
+
     cdisc_json = cdisc_bc_library.to_cdisc_json(item)
     with open(f"bc_data/cdisc/{filename}.json", 'w', encoding='utf-8') as f:
       json.dump(cdisc_json, f, indent=2)
+
     usdm_json = cdisc_bc_library.to_usdm_json(item)
     with open(f"bc_data/usdm/{filename}.json", 'w', encoding='utf-8') as f:
       json.dump(json.loads(usdm_json), f, indent=2)
-    print("BC:", name)
+
+    print(f"{index + 1}. BC: {name}")
+    
+  with open(f"bc_data/catalogue.yaml", 'w', encoding='utf-8') as f:
+    yaml.dump(bc_list, f, default_flow_style=False)
