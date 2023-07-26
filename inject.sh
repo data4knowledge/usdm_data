8719e01ea682bf2f8063496b6a25066baf8e66d7
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-07-26 15:07:05 +0200

    Simple inject test script

---
+++ inject.sh
@@ -0,0 +1,3 @@
+python inject.py http://localhost:8000/v2/ NCT04320615/Roche_NCT04320615_COVID
+python inject.py http://localhost:8000/v2/ NCT03421379/EliLilly_NCT03421379_Diabetes
+python inject.py http://localhost:8000/v2/ CDISC_Pilot/CDISC_Pilot_Study
