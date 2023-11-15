156540f89baa45e19df23e437cfd4ee6ae72eb32
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-11-15 10:50:05 +0100

    Tweak to test file and code

--- test.py
+++ test.py
@@ -32,10 +32,6 @@ if __name__ == "__main__":
 
     filename = sys.argv[1].strip()
     excel = USDMExcel(f"{filename}.xlsx")
-    errors = excel.errors()
-    if len(errors) > 0:
-      with open(f"{filename}_errors.yaml", 'w', encoding='utf-8') as f:
-        yaml.dump(errors, f, default_flow_style=False)
     with open(f"{filename}.json", 'w', encoding='utf-8') as f:
       f.write(json.dumps(json.loads(excel.to_json()), indent=2))
     with open(f"{filename}_USDM.html", 'w', encoding='utf-8') as f:
@@ -44,6 +40,10 @@ if __name__ == "__main__":
       f.write(excel.to_pdf())
     with open(f"{filename}_timeline.html", 'w') as f:
       f.write(excel.to_timeline())
+    errors = excel.errors()
+    if len(errors) > 0:
+      with open(f"{filename}_errors.yaml", 'w', encoding='utf-8') as f:
+        yaml.dump(errors, f, default_flow_style=False)
 
   else:
     print("Multiple command line arguments detected.")
