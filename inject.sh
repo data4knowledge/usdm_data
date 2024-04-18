7a2fd7a1830ed0cc8b938298aa850f0824801d00
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-04-18 09:20:59 +0200

    Fix script

--- inject.sh
+++ inject.sh
@@ -1,5 +1,5 @@
-python inject.py source_data/NCT03421379/EliLilly_NCT03421379_Diabetes http://localhost:8000/v3/
-python inject.py source_data/CDISC_Pilot/CDISC_Pilot_Study http://localhost:8000/v3/
+python inject.py source_data/protocols/NCT03421379/EliLilly_NCT03421379_Diabetes http://localhost:8000/v3/
+python inject.py source_data/protocols/CDISC_Pilot/CDISC_Pilot_Study http://localhost:8000/v3/
 
-python inject.py source_data/Errors/EliLilly_NCT03421379_Diabetes_Error http://localhost:8000/v3/
-python inject.py source_data/Errors/CDISC_Pilot_Study_Error http://localhost:8000/v3/
+python inject.py source_data/errors/EliLilly_NCT03421379_Diabetes_Error http://localhost:8000/v3/
+python inject.py source_data/errors/CDISC_Pilot_Study_Error http://localhost:8000/v3/
