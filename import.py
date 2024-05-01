981d6622f0cf19d7444c3af7a18b07b27ab13488
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-05-01 15:50:19 +0200

    Update import program for FHIR output

--- import.py
+++ import.py
@@ -22,8 +22,9 @@ def save_as_pdf_file(data, details, suffix):
   with open(f"{ROOT_PATH}{details['path']}/{details['filename']}_{suffix}.pdf", 'w+b') as f:
     f.write(data)
 
-def save_as_json_file(raw_json, details):
-  with open(f"{ROOT_PATH}{details['path']}/{details['filename']}.json", 'w', encoding='utf-8') as f:
+def save_as_json_file(raw_json, details, suffix=''):
+  filename = f"{ROOT_PATH}{details['path']}/{details['filename']}_{suffix}.json" if suffix else f"{ROOT_PATH}{details['path']}/{details['filename']}.json"
+  with open(filename, 'w', encoding='utf-8') as f:
     json_object = json.loads(raw_json)
     json.dump(json_object, f, indent=2)
 
@@ -63,11 +64,6 @@ for study in studies:
   save_as_csv_file(errors, study)
   print("Timeline\n\n")
   save_as_html_file(x.to_timeline(), study, 'timeline')
-  # for view in [USDMDb.FULL_VIEW, USDMDb.TIMELINE_VIEW]:
-  #   print(f"{str(view)} view\n\n")
-  #   nodes, edges = x.to_nodes_and_edges(view)
-  #   save_as_node_file(nodes, study, view)
-  #   save_as_edges_file(edges, study, view)
   if study['protocol']:
     print(f"\n\nProtocol HTML and PDF (watermark={study['watermark']}, highlight={study['highlight']})\n\n")
     if study['highlight']:
@@ -75,5 +71,7 @@ for study in studies:
     save_as_html_file(x.to_html(), study, 'USDM')
     save_as_html_file(x.to_timeline(), study, 'timeline')
     save_as_pdf_file(x.to_pdf(study['watermark']), study, 'USDM')
+    if study['m11']:
+      save_as_json_file(x.to_fhir(), study, 'fhir')
   print(f"\n\nERRORS:\n{errors}\n\n")
   print(f"----- + -----\n\n")
