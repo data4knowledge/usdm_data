80cec0b2e417c2ce0bf34456282cbab6c40d01a0
Dave Iberson-Hurst <daveih@DIH-M1-MINI.home>
2024-05-02 09:50:06 +0200

    Update test program for using template override

--- test.py
+++ test.py
@@ -2,25 +2,33 @@ import argparse
 import logging
 import json
 import csv
+import os
 
-def save_as_csv_file(errors, filename):
-  with open(f"{filename}.csv", 'w', encoding='utf-8') as f:
+def save_as_csv_file(errors, output_path, filename):
+  full_filename = os.path.join(output_path, f"{filename}.csv")
+  print(f"CSV: {full_filename}")
+  with open(full_filename, 'w', encoding='utf-8') as f:
     writer = csv.DictWriter(f, fieldnames=['sheet','row','column','message','level'])
     writer.writeheader()
     writer.writerows(errors)
 
-def save_as_json_file(raw_json, filename, suffix=''):
-  filename = f"{filename}_{suffix}.json" if suffix else f"{filename}.json"
-  with open(f"{filename}", 'w', encoding='utf-8') as f:
+def save_as_json_file(raw_json, output_path, filename, suffix=''):
+  full_filename = os.path.join(output_path, f"{filename}_{suffix}.json" if suffix else f"{filename}.json")
+  print(f"JSON: {full_filename}")
+  with open(full_filename, 'w', encoding='utf-8') as f:
     json_object = json.loads(raw_json)
     json.dump(json_object, f, indent=2)
 
-def save_as_html_file(html, filename, suffix):
-  with open(f"{filename}_{suffix}.html", 'w', encoding='utf-8') as f:
+def save_as_html_file(html, output_path, filename, suffix):
+  full_filename = os.path.join(output_path, f"{filename}_{suffix}.html")
+  print(f"HTML: {full_filename}")
+  with open(full_filename, 'w', encoding='utf-8') as f:
     f.write(html)
 
-def save_as_pdf_file(data, filename, suffix):
-  with open(f"{filename}_{suffix}.pdf", 'w+b') as f:
+def save_as_pdf_file(data, output_path, filename, suffix):
+  full_filename = os.path.join(output_path, f"{filename}_{suffix}.pdf")
+  print(f"PDF: {full_filename}")
+  with open(full_filename, 'w+b') as f:
     f.write(data)
 
 if __name__ == "__main__":
@@ -29,20 +37,27 @@ if __name__ == "__main__":
     description='Will take the USDM conformant Excel workbook and transform into USDM JSON, HTML, PDF etc',
     epilog='Note: Not that sophisticated! :)'
   )
-  parser.add_argument('filename', help="The name of the Excel file without the '.xlsx' extension.") 
-  parser.add_argument("--no_watermark", action="store_true", help="No watermark, default is to show watermark") 
-  parser.add_argument("--highlight", action="store_false", help="Highlight USDM content, default is not to highlight") 
-  parser.add_argument("--m11", action="store_false", help="Generate the M11 fhir message as JSON") 
-  parser.add_argument('--debug', action='store_true', help='print debug messages to stderr')
+  parser.add_argument('filename', help="The name of the Excel file.") 
+  parser.add_argument('-o', '--out', help="The path of where the output files should be written. Defaults to the input path.", default=None) 
+  parser.add_argument('-t', "--template", help="Override the configured template with the specified one", default=None) 
+  parser.add_argument('-nw', "--no_watermark", action="store_true", help="No watermark, default is to show the watermark") 
+  parser.add_argument('-hl', "--highlight", action="store_false", help="Highlight USDM content, default is not to highlight") 
+  parser.add_argument('-d', '--debug', action='store_true', help='print debug messages to stderr')
   args = parser.parse_args()
   filename = args.filename
+  output_path = args.out
+  template = args.template
   watermark = not args.no_watermark
   highlight = not args.highlight
-  m11 = not args.m11
   debug = args.debug
   level = logging.DEBUG if debug else logging.INFO
 
-  full_filename = f"{filename}.xlsx"
+  #full_filename = f"{filename}.xlsx"
+  #full_filename = os.path.join(input_path, f"{filename}.xlsx")
+  input_path, tail = os.path.split(filename)
+  root_filename = tail.replace(".xlsx", "")
+  full_filename = filename
+  output_path = output_path if output_path else input_path
   log = logging.basicConfig(level=level)
   
   from usdm_db import USDMDb
@@ -51,18 +66,22 @@ if __name__ == "__main__":
   
   print("")
   print(f"Test Utility, using USDM Python Package v{code_version} supporting USDM version v{model_version}")
-  print(f"Converting {full_filename} with watermark set {'on' if watermark else 'off'}, highlights {'on' if highlight else 'off'}, M11 FHIR message set {'on' if m11 else 'off'}")
+  print(f"Converting {full_filename} with watermark set {'on' if watermark else 'off'}, highlights {'on' if highlight else 'off'}")
+  print(f"Output path is: {output_path}")
+  if template:
+    print(f"Overiding configured template with: {template}")
   print("")
   print("")
 
   usdm = USDMDb()
-  errors = usdm.from_excel(full_filename)
-  save_as_json_file(json.dumps(json.loads(usdm.to_json())), filename)
-  save_as_csv_file(errors, filename)
+  errors = usdm.from_excel(full_filename, template)
+  save_as_json_file(json.dumps(json.loads(usdm.to_json())), output_path, root_filename)
+  save_as_csv_file(errors, output_path, root_filename)
   if highlight:
-    save_as_html_file(usdm.to_html(highlight), filename, 'highlight')
-  save_as_html_file(usdm.to_html(), filename, 'USDM')
-  save_as_pdf_file(usdm.to_pdf(watermark), filename, 'USDM')
-  save_as_html_file(usdm.to_timeline(), filename, 'timeline')
-  if m11:
-    save_as_json_file(usdm.to_fhir(), filename, 'fhir')
+    save_as_html_file(usdm.to_html(highlight), output_path, root_filename, 'highlight')
+  save_as_html_file(usdm.to_html(), output_path, root_filename, 'USDM')
+  save_as_pdf_file(usdm.to_pdf(watermark), output_path, root_filename, 'USDM')
+  save_as_html_file(usdm.to_timeline(), output_path, root_filename, 'timeline')
+  print(f"M11: {usdm.was_m11()}")
+  if usdm.was_m11():
+    save_as_json_file(usdm.to_fhir(), output_path, root_filename, 'fhir')
