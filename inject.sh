27f2691fc960f9a25a22fa86440f9640127a5e52
Dave Iberson-Hurst <daveih@dih-m1-mini-1498773ddc71.home>
2024-08-28 09:30:49 +0200

    Update inject script for dir changes

--- inject.sh
+++ inject.sh
@@ -1,5 +1,5 @@
-python inject.py source_data/protocols/NCT03421379/EliLilly_NCT03421379_Diabetes http://localhost:8004/v3/
-python inject.py source_data/protocols/NCT03421379/EliLilly_NCT03421379_Diabetes http://localhost:8004/v3/
+python inject.py source_data/protocols/NCT03421379_EliLilly_Diabetes/EliLilly_NCT03421379_Diabetes http://localhost:8004/v3/
+python inject.py source_data/protocols/NCT04573309_Alexion_Wilsons/NCT04573309 http://localhost:8004/v3/
 python inject.py source_data/protocols/CDISC_Pilot/CDISC_Pilot_Study http://localhost:8004/v3/
 
 #python inject.py source_data/errors/EliLilly_NCT03421379_Diabetes_Error http://localhost:8004/v3/
