9e13092cba5ced48a77399540b47328630977ba9
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-03-01 11:41:11 +0100

    Update inject test script

--- inject.sh
+++ inject.sh
@@ -1,6 +1,8 @@
-python inject.py source_data/protocols/NCT03421379_EliLilly_Diabetes/EliLilly_NCT03421379_Diabetes http://localhost:8004/v3/
-python inject.py source_data/protocols/NCT04573309_Alexion_Wilsons/NCT04573309 http://localhost:8004/v3/
+# Valid
+python inject.py source_data/protocols/EliLilly_NCT03421379_Diabetes/EliLilly_NCT03421379_Diabetes http://localhost:8004/v3/
+python inject.py source_data/protocols/Alexion_NCT04573309_Wilsons/Alexion_NCT04573309_Wilsons http://localhost:8004/v3/
 python inject.py source_data/protocols/CDISC_Pilot/CDISC_Pilot_Study http://localhost:8004/v3/
 
-#python inject.py source_data/errors/EliLilly_NCT03421379_Diabetes_Error http://localhost:8004/v3/
-#python inject.py source_data/errors/CDISC_Pilot_Study_Error http://localhost:8004/v3/
+# Errors
+python inject.py source_data/errors/EliLilly_NCT03421379_Diabetes_Error http://localhost:8004/v3/
+python inject.py source_data/errors/CDISC_Pilot_Study_Error http://localhost:8004/v3/
