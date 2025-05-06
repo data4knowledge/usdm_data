fd1f88298f769977d0305fa21e57755f2461995c
daveih <daveih1664dk@gmail.com>
2025-05-06 09:32:48 +0200

    Update results for update in USDM package for durations

--- inject.sh
+++ inject.sh
@@ -2,6 +2,8 @@
 python inject.py source_data/protocols/EliLilly_NCT03421379_Diabetes/EliLilly_NCT03421379_Diabetes http://localhost:8000/v3/
 python inject.py source_data/protocols/Alexion_NCT04573309_Wilsons/Alexion_NCT04573309_Wilsons http://localhost:8000/v3/
 python inject.py source_data/protocols/CDISC_Pilot/CDISC_Pilot_Study http://localhost:8000/v3/
+python inject.py source_data/temporary/devices/devices http://localhost:8000/v3/
+python inject.py source_data/temporary/observational/observational http://localhost:8000/v3/
 
 # Errors
 python inject.py source_data/errors/EliLilly_NCT03421379_Diabetes_Error http://localhost:8000/v3/
